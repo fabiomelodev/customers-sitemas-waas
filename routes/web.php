f4098352a9680ee686ba8537633772326e3339c3
fabiomelodev <fabiomelodev@gmail.com>
2026-04-29 22:15:31 -0300

    fix: rota para validar token enviado para o sitemas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/autologin/{token}', function ($token) {
+    $user = User::query()->where('login_token', $token)->firstOrFail();
+
+    // Loga o usuário na aplicação app.
+    auth()->login($user);
+
+    // Apaga o token para ele nunca mais ser usado
+    $user->update(['login_token' => null]);
+
+    // Manda para o Dashboard do Filament
+    return redirect('/admin');
+})->name('autologin');
